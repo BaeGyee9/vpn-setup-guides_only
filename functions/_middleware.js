f8667621b46a56d254562ece96ce9cab45b53237
BaeGyee9 <zawtunwaiz@gmail.com>
2025-08-21 04:23:24 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -248,3 +248,4 @@ export async function onRequest(context) {
         return new Response("This is a Telegram bot webhook endpoint. Please send POST requests or access /registerWebhook or /unregisterWebhook.", { status: 200 });
     }
 }
+
